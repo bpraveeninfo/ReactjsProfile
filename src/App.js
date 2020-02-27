2522008b0c2bb7b99a75091703cb04502c50847f
bpraveen.info <bpraveen.info@gmail.com>
2020-02-27 19:02:42 +0530

    ADDED routers boorstap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-
+    
+      import { Button, ButtonToolbar, Breadcrumb, Jumbotron} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    
+    <Breadcrumb>
+      <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+      <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+        Library
+      </Breadcrumb.Item>
+      <Breadcrumb.Item active>Data</Breadcrumb.Item>
+    </Breadcrumb>
+    <Jumbotron>
+    <h1>welcome to our website</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for calling
+        extra attention to featured content or information.
+      </p>
+      <p>
+        <Button variant="primary">Learn more</Button>
+      </p>
+    </Jumbotron>
+    <ButtonToolbar>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </ButtonToolbar>
     </div>
+
   );
 }
 
